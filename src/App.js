94a75cb67ba09a0eb92d99702e4079bedf46a28c
Justen Schilinski <justenschilinski34@hotmail.com>
2022-12-20 16:43:00 -0500

    Added two bootstrap cards and cleaned up CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import Card from 'react-bootstrap/Card';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-            
+      <header className="App-header"> 
+        <Card bg={'info'}>
+          <Card.Body>
+            <Card.Text>This is some text</Card.Text>
+          </Card.Body>
+        </Card>     
       </header>
+      <body className="App-body">
+        <Card bg={'info'}>
+          <Card.Body>
+            <Card.Text>This is some text</Card.Text>
+          </Card.Body>
+        </Card>
+      </body>
     </div>
   );
 }
