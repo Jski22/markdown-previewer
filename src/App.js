799b59532ca58570cd64f348c7a40bac02a92b69
Justen Schilinski <justenschilinski34@hotmail.com>
2022-12-23 23:46:15 -0500

    Added state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import Card from 'react-bootstrap/Card';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"> 
-        <Card bg={'info'}>
-          <Card.Body>
-            <Card.Text>This is some text</Card.Text>
-          </Card.Body>
-        </Card>     
-      </header>
-      <body className="App-body">
-        <Card bg={'info'}>
-          <Card.Body>
-            <Card.Text>This is some text</Card.Text>
-          </Card.Body>
-        </Card>
-      </body>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: " "
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"> 
+          <Card bg={'info'}>
+            <Card.Body>
+              <Card.Text>This is some text</Card.Text>
+            </Card.Body>
+          </Card>     
+        </header>
+        <body className="App-body">
+          <Card bg={'info'}>
+            <Card.Body>
+              <Card.Text>This is some text</Card.Text>
+            </Card.Body>
+          </Card>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
