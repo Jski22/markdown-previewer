fd1d6e6f3402f763d807f095c4da9db03a6a0891
Justen Schilinski <justenschilinski34@hotmail.com>
2023-01-03 16:38:18 -0500

    Simplified inputs to one textarea and stylized markdown output

--- src/App.js
+++ src/App.js
@@ -9,13 +9,38 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      input1: "# Welcome to my React Markdown Previewer!",
-      input2: "## This is a sub-heading",
-      input3: "### And here's some other cool stuff:",
-      input4: "Here's some code, `<div></div>`, between 2 backticks.",
-      input5: "``` \n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\nif (firstLine == '```' && lastLine == '```') {\nreturn multiLineCode;\n  }\n}\n```",
-      input6: "You can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.",
-      input7: "There's also [links](https://www.freecodecamp.org), and\n  > Block Quotes!"
+      input: (`# Welcome to my React Markdown Previewer!
+## This is a sub-heading
+### And here's some other cool stuff:
+Here's some code, \`<div></div>\`, between 2 backticks.
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode; 
+    }
+  }
+\`\`\`
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.markdownguide.org/), and
+  > Block Quotes!
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here...
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+![Markdown Logo](https://cdn.icon-icons.com/icons2/2389/PNG/512/markdown_logo_icon_145085.png)`),
     };
     this.handleChange = this.handleChange.bind(this);
   };
@@ -34,25 +59,7 @@ class App extends React.Component {
             <Card.Header>Editor</Card.Header>
             <Card.Body>
               <Row>
-                <input value={this.state.input1} onChange={this.handleChange}  />
-              </Row>
-              <Row>
-                <input value={this.state.input2} onChange={this.handleChange}  />
-              </Row>
-              <Row>
-                <input value={this.state.input3} onChange={this.handleChange}  />
-              </Row>
-              <Row>
-                <textarea value={this.state.input4} onChange={this.handleChange}  />
-              </Row>
-              <Row>
-                <textarea value={this.state.input5} onChange={this.handleChange}  />
-              </Row>
-              <Row>
-                <textarea value={this.state.input6} onChange={this.handleChange}  />
-              </Row>
-              <Row>
-                <textarea value={this.state.input7} onChange={this.handleChange}  />
+                <textarea value={this.state.input} onChange={this.handleChange}  />
               </Row>
             </Card.Body>
           </Card>     
@@ -61,13 +68,7 @@ class App extends React.Component {
           <Card bg={'info'} id="previewer">
             <Card.Header>Previewer</Card.Header>
             <Card.Body>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input1)}}></Card.Text>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input2)}}></Card.Text>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input3)}}></Card.Text>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input4)}}></Card.Text>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input5)}}></Card.Text>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input6)}}></Card.Text>
-              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input7)}}></Card.Text>
+              <Card.Text dangerouslySetInnerHTML={{__html: marked(this.state.input)}}></Card.Text>
             </Card.Body>
           </Card>
         </body>
